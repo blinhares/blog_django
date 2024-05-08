68eecba45c16a623fe009114254c511b6afa9c33
bruno <b_linhares@outlook.com>
2024-05-08 09:20:51 -0300

    feat: criar uma vie para pagina index

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(requests:HttpRequest)->HttpResponse:
+    return render(
+        requests,
+        'blog/pages/index.html'
+    )
+
