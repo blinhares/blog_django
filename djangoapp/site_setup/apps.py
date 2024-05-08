809b878e0a6c48106da6389eb0ace1a341aa54fe
bruno <b_linhares@outlook.com>
2024-05-08 15:36:12 -0300

    feat: criando app de setup da pagina

---
+++ djangoapp/site_setup/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SiteSetupConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'site_setup'
+    verbose_name = "Configurações Site"
