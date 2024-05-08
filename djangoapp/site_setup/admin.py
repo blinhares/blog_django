bb36c69a6abba9d4f271b189888a0c29897a3524
bruno <b_linhares@outlook.com>
2024-05-08 17:17:32 -0300

    feat: add um model dentro de outro na area adm

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -4,13 +4,22 @@ from django.http import HttpRequest
 
 @admin.register(MenuLink)
 class MenuLinkAdmin(admin.ModelAdmin): # type: ignore
-    list_display = 'id', 'text', 'url_or_path'
+    list_display = 'id', 'text', 'url_or_path', 'site_setup'
     search_fields = list_display
     list_display_links = list_display
+
+class MenuLinkInLine(admin.TabularInline): # type: ignore
+    """
+    Cria uma visualizacao dos dados inline para ser inserido em outra exibicao
+    Pode-se usar o admin.TabularInline ou admin.StackedInline como opcoes visuais
+    """
+    model = MenuLink
+    extra = 1
 
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin): # type: ignore
     list_display = 'title', 'description',
+    inlines = MenuLinkInLine,
 
     def has_add_permission(self, request:HttpRequest) -> bool:
         """Esse metodo reescrito de `admin.ModelAdmin`.
