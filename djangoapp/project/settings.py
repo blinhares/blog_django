320dce1049578b7c21ba69ac29545063d108f469
bruno <b_linhares@outlook.com>
2024-05-08 15:36:32 -0300

    feat: add app ao projeto

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog' #add o app criado
+    'blog', #add o app criado
+    'site_setup',
 ]
 
 MIDDLEWARE = [
