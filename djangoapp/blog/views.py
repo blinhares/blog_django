246b6c502cdcc8ab155d1635070bb89124d8cd94
bruno <b_linhares@outlook.com>
2024-05-09 17:22:40 -0300

    feat: add views de post e pages

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,10 +1,46 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+posts = list(range(1000))
 
 # Create your views here.
-def index(requests:HttpRequest)->HttpResponse:
+def index(request:HttpRequest)->HttpResponse:
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
-        requests,
-        'blog/pages/index.html'
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
     )
 
+def page(request:HttpRequest)->HttpResponse:
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/page.html',
+        {
+            # 'page_obj': page_obj,
+        }
+    )
+
+
+def post(request:HttpRequest)->HttpResponse:
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/post.html',
+        {
+            # 'page_obj': page_obj,
+        }
+    )
