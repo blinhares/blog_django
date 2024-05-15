c889987b69771f24f254b59e2fdfcfa7cbc196d4
bruno <b_linhares@outlook.com>
2024-05-15 12:05:13 -0300

    feat: migrando view category

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('page/<slug:slug>/', page, name='page'),
     # path('created_by/<int:user_id>/', created_by, name='created_by'),
     path('created_by/<int:user_id>/', CreatedBy.as_view(), name='created_by'),
-    path('by_category/<slug:category_slug>/', by_category, name='by_category'),
+    # path('by_category/<slug:category_slug>/', by_category, name='by_category'),
+    path('by_category/<slug:category_slug>/', CategoryListView.as_view(), name='by_category'),
     path('by_tag/<slug:tag_slug>/', by_tag, name='by_tag'),
     path('search/', search, name='search'),
 ]
