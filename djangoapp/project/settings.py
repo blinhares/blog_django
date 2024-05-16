c664c3df4a99801840a9f4df33f48f20e9532201
bruno <b_linhares@outlook.com>
2024-05-15 23:40:36 -0300

    clear statics

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -34,10 +34,10 @@ SECRET_KEY = os.getenv('SECRET_KEY','change-me')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv('DEBUG',0)))
 
-# ALLOWED_HOSTS = [
-#     h.strip() for h in os.getenv('ALLOWED_HOSTS','').split(',')
-#     if h.strip()
-# ]
+ALLOWED_HOSTS = [
+    h.strip() for h in os.getenv('ALLOWED_HOSTS','').split(',')
+    if h.strip()
+]
 
 # Application definition
 
@@ -136,8 +136,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-##### STATIC_URL = '/static/'
-##### STATIC_ROOT = DATA_DIR / 'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = DATA_DIR / 'static'
 
 # Media files 
 MEDIA_URL = '/media/'
